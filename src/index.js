3bca603b2c45c073e04c28fc0bfef35444681a15
KarinaMacedo13 <ruth.macedo@ucsp.edu.pe>
2022-02-11 11:58:55 -0500

    index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import validator from './validator.js';
 
+const btnvalid = document.getElementById('btn');
+btnvalid.addEventListener('click',btnclick);
+
+function btnclick (){
+let numbercardvalid = document.getElementById("numbercard").value;
+let respuesta = document.getElementById("respuesta");
+let respuesta2 = document.getElementById("respuesta2");
+let numbermask = validator.maskify(numbercardvalid);
+respuesta.innerHTML = "Tu tarjeta es: "+numbermask;
+if (validator.isValid(numbercardvalid)==true){
+  respuesta2.innerHTML = "Tu tarjeta es valida";
+}else{
+  respuesta2.innerHTML = "Tu tarjeta es invalida";
+}
+}
+
 console.log(validator);
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
+  isValid:function(numbercardvalid){
+    let arraycard= Array.from(numbercardvalid);
+    let arraycard2=[];
+    let suma = 0;
+    for (let i= 0, l = arraycard.length; i < l; i = i + 1) {
+      arraycard2.push(parseInt(arraycard[i]));
+    }
+    let numreverse = arraycard2.reverse();
+
+    for (let i = 0, l = numreverse.length; i < l; i = i + 1) {
+      if (i%2!==0){
+        numreverse[i] *= 2;
+        if (numreverse[i] >= 10) {
+          numreverse[i] = (numreverse[i] % 10) + 1;
+        }
+      }
+      suma += numreverse[i];
+    }
+    if (suma % 10 == 0 || suma == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  maskify:function(numbercardvalid){
+    let n=4;
+    let mascara="#";
+    let newnumber="";
+    newnumber=(""+numbercardvalid).slice(0,-n).replace(/./g,mascara)+(""+numbercardvalid).slice(-n);
+    return newnumber;
+  },
 };
 
 export default validator;
