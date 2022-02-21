8b8da2e3dc5916bede12b9e519aca6ed7ba64698
KarinaMacedo13 <ruth.macedo@ucsp.edu.pe>
2022-02-20 19:34:19 -0500

    5 commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import validator from './validator.js';
 const formCard=document.querySelector("#formCard"),
 numberCard=document.querySelector(".number"),
 name=document.querySelector(".name"),
-logoBrand=document.querySelector("#logoBrand");
+logoBrand=document.querySelector("#logoBrand"),
+mistake=document.querySelector("#mistake");
 
 //input numberCard
 formCard.inputNumber.addEventListener("keyup", (e)=>{
@@ -14,9 +15,9 @@ formCard.inputNumber.addEventListener("keyup", (e)=>{
   .replace(/\s/g,"")
   //Eliminamos las letras
   .replace(/\D/g,"")
-  numberCard.textContent=numbermask;
+  numberCard.innerHTML=numbermask;
   if(cardNumber==""){
-    numberCard.textContent="#### #### #### ####";
+    numberCard.innerHTML="#### #### #### ####";
     logoBrand.innerHTML=" ";
   }
   let issuer = validator.getIssuer(cardNumber);
@@ -35,22 +36,25 @@ formCard.inputNumber.addEventListener("keyup", (e)=>{
 //input cardname
 formCard.inputName.addEventListener("keyup", (e)=>{
   let cardName=e.target.value;
-  name.textContent=cardName;
+  name.innerHTML=cardName;
   formCard.inputName.value=cardName
   //Eliminamos los numeros
   .replace(/\d/g,"")
   if(cardName==""){
-    name.textContent="Ingrese Nombre";
+    name.innerHTML="Ingrese Nombre";
   }
 });
 
 const btnSend=document.querySelector("#btnSend");
-btnSend.addEventListener("click", ()=>{
+btnSend.addEventListener("click", (e)=>{
+  e.preventDefault();
   let inputNumber=document.querySelector("#inputNumber").value;
   let inputName=document.querySelector("#inputName").value;
   let valid=document.getElementById("valid");
   if (inputNumber=="" && inputName==""||inputNumber==""||inputName==""){
-    alert("Ingrese sus datos");
+    mistake.innerHTML="Ingrese sus datos";
+    document.querySelector(".mistake").style.display="block";
+    //alert("Ingrese sus datos");
   } else if (validator.isValid(inputNumber)==true){
     valid.innerHTML = "Tu tarjeta es válida";
     document.getElementById("screen2").style.display="block";
@@ -64,8 +68,9 @@ btnSend.addEventListener("click", ()=>{
 
 const btnReturn=document.getElementById("btnReturn");
 btnReturn.addEventListener("click", ()=>{
+  document.getElementById('formCard').reset();
+  document.querySelector(".mistake").style.display="none";
   document.getElementById("screen2").style.display="none";
   document.getElementById("screen1").style.display="block";
 });
-
 
--- src/validator.js
+++ src/validator.js
@@ -2,7 +2,7 @@ const validator = {
   isValid:function(numbercardvalid){
     let arraycard= Array.from(numbercardvalid);
     let arraycard2=[];
-    let suma = 0;
+    let plus = 0;
     for (let i= 0, l = arraycard.length; i < l; i = i + 1) {
       arraycard2.push(parseInt(arraycard[i]));
     }
@@ -15,9 +15,9 @@ const validator = {
           numreverse[i] = (numreverse[i] % 10) + 1;
         }
       }
-      suma += numreverse[i];
+      plus += numreverse[i];
     }
-    if (suma % 10 == 0 || suma == 0) {
+    if (plus % 10 == 0) {
       return true;
     } else {
       return false;
@@ -25,24 +25,21 @@ const validator = {
   },
 
   maskify:function(numbercardvalid){
-    let n=4;
-    let mascara="#";
-    let newnumber="";
-    newnumber=(""+numbercardvalid).slice(0,-n).replace(/./g,mascara)+(""+numbercardvalid).slice(-n);
+    let n=4,
+    mask="#",
+    newnumber="";
+    newnumber=(""+numbercardvalid).slice(0,-n).replace(/./g,mask)+(""+numbercardvalid).slice(-n);
     return newnumber;
   },
 
   getIssuer:function(numbercardvalid){
     if(numbercardvalid[0]==4){
-      let visa="visa";
-      return visa;
+      return "visa";
 
     } else if(numbercardvalid[0]==5&&numbercardvalid[1]==1||numbercardvalid[0]==5&&numbercardvalid[1]==5){
-      let mastercard="mastercard";
-      return mastercard;
+      return "mastercard";
     }
   },
-
 };
 
 export default validator;
